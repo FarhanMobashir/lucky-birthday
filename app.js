dcb0fce36b536d7c3baab0758fa9abf3a0d9d010
Mobashir Farhan <farhanmobashir007@gmail.com>
2021-09-01 01:13:27 +0530

    adds basic functonality to the app

---
+++ app.js
@@ -0,0 +1,42 @@
+const birthdateInput = document.querySelector(".birthdate-input");
+const luckyNumberInput = document.querySelector(".lucky-number-input");
+const checkButton = document.querySelector(".check-button");
+
+const output = document.querySelector(".output");
+
+let userBirthdate = "";
+let luckyNumber = "";
+
+birthdateInput.addEventListener("change", function (e) {
+  userBirthdate = e.target.value;
+});
+
+luckyNumberInput.addEventListener("change", function (e) {
+  luckyNumber = e.target.value;
+});
+
+function isLucky(date, luckyNumber) {
+  let sumOfBdayDigits = 0;
+  date = date.split("-").join("").split("");
+  for (let i = 0; i < date.length; i++) {
+    sumOfBdayDigits += Number(date[i]);
+  }
+  console.log("Sum", sumOfBdayDigits, "luckyNumber", parseInt(luckyNumber));
+  if (sumOfBdayDigits % Number(luckyNumber) === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(isLucky("10-10-2002", 3));
+
+checkButton.addEventListener("click", function () {
+  if (userBirthdate !== "" && luckyNumber !== "") {
+    if (isLucky(userBirthdate, luckyNumber) === true) {
+      output.innerText = `Wooho your birthdate is a lucky one`;
+    } else if (isLucky(userBirthdate, luckyNumber) === false) {
+      output.innerText = `Sorry , your birthdate is not a lucky one`;
+    }
+  }
+});
